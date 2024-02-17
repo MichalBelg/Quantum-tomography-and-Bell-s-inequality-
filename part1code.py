4943541962b16b32fcad11dc351a587ec02e8824
MichalBelg <159570130+MichalBelg@users.noreply.github.com>
2024-02-17 18:20:03 +0200

    Delete part1code.py

--- part1code.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def generate_random_bits(length):
-    return ''.join([random.choice('01') for _ in range(length)])
-
-for i in [10,25,50]:
-    random_bits = generate_random_bits(i)
-    print(random_bits)
