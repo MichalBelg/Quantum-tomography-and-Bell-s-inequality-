f32e9a2028ec7c1ecffaff5cb92825624c32c105
MichalBelg <159570130+MichalBelg@users.noreply.github.com>
2024-02-24 15:44:47 +0200

    Update part1code.py

--- part1code.py
+++ part1code.py
@@ -3,6 +3,6 @@ import random
 def generate_random_bits(length):
     return ''.join([random.choice('01') for _ in range(length)])
 
-for i in [10,25,50]:
+for i in [100,500,1000]:
     random_bits = generate_random_bits(i)
     print(random_bits)
